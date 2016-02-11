0d68c2ad4e55cf0550b604d9a484ee4974c6562d
Júnior Pacheco <pachecofaculdade@gmail.com>
2016-02-11 17:25:48 -0200

    Principais funções

---
+++ FirstPython.py
@@ -0,0 +1,28 @@
+sentence = 'Ola meu amigo, Como vai essa vida'
+print(sentence)
+
+vegetable = 'Aspargus'
+
+print(vegetable)
+print(vegetable[0])
+print(len(vegetable))
+print(vegetable.upper())
+print(vegetable.lower())
+
+#String Concatenation
+print('Python e ' + ' legal' +'. Eu estou ' + 'gostando')
+first = 'Python'
+second = 'is'
+third = 'fun'
+sentence = first + ' ' + second + ' ' + third + '.'
+print(sentence)
+
+#Repeating Strings
+print('-' * 10)
+
+#str() function
+version = 3
+print('Python ' + str(version) + ' is fun')
+
+#formatting string
+print('Eu sou mais {} do que ele.'.format('alto'))
